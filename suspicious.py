3f884a3395b38985844865ed308e14c708440559
MrNeutrino <32274898+MrNeutrino@users.noreply.github.com>
2017-10-07 22:25:05 +0100

    Update suspicious.py

--- suspicious.py
+++ suspicious.py
@@ -4,7 +4,7 @@ argv_one = argv[1] # file one - order is irrelevant
 argv_two = argv[2] # file two
 
 with open(argv_one) as one, open(argv_two) as two:              	# open both text files
-	one, two = one.read().splitlines(), two.read().splitlines()	# create two lists
+	one, two = one.read().splitlines(), two.read().splitlines()	# .splitlines() creates two lists
 	intersection = sorted(list(set(one)&set(two)))			# check duplicates, put into list, sort
 	for line in intersection:					# print one name per line
 		print(line)
