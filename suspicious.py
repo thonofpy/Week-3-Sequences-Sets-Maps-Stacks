f42499211b21aa114987035b85e367d4afce269b
MrNeutrino <32274898+MrNeutrino@users.noreply.github.com>
2017-10-07 22:14:17 +0100

    Update suspicious.py

--- suspicious.py
+++ suspicious.py
@@ -3,8 +3,8 @@ from sys import argv
 argv_one = argv[1] # students
 argv_two = argv[2] # delinquents
 
-with open(argv_one) as one, open(argv_two) as two:              # open both text files
+with open(argv_one) as one, open(argv_two) as two:              	# open both text files
 	one, two = one.read().splitlines(), two.read().splitlines()	# create two lists
-	intersection = sorted(list(set(one)&set(two)))				# check duplicates, put into list, sort
-	for line in intersection:									# print one name per line
+	intersection = sorted(list(set(one)&set(two)))			# check duplicates, put into list, sort
+	for line in intersection:					# print one name per line
 		print(line)
