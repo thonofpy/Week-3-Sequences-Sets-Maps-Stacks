7f236ac6e4d3e354d380d982233a9e19ae447728
MrNeutrino <32274898+MrNeutrino@users.noreply.github.com>
2017-10-10 16:46:16 +0100

    Update reverse_input.py

--- reverse_input.py
+++ reverse_input.py
@@ -1,11 +1,12 @@
 import sys
+from Stack import Stack
 
 def reverse_input(stack):
-	lines = sys.stdin.readlines()
-	a = []
-	for value in lines:
-		a.append(value.strip())
-	for v in a:
-		stack.push(a)
-	while not stack.is_empty():
-		print(stack.pop().pop())
+	text = sys.stdin.read().splitlines()
+	line = text.pop()
+	while line is not None:
+		print(line)
+		try:
+			line = text.pop()
+		except IndexError:
+			break
